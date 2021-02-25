127acdd7471cdc2f3de80b717d28c9b34a0e0e17
Scott Robertson <scottymeuk@gmail.com>
2021-02-25 01:28:25 +0000

    Releasing 0.8.2

--- Casks/balancebar.rb
+++ Casks/balancebar.rb
@@ -1,6 +1,6 @@
 cask "balancebar" do
-  version "0.8.1"
-  sha256 "5170e594f0ecd87a383db0d1f2d3eda72dfc87b2466559dd5bfc7412c12aa22e"
+  version "0.8.2"
+  sha256 "8f1c8b7275f98b62bd4ec92531365a9eba6bd94fc525e539202fa568ed684293"
   url "https://github.com/scottrobertson/balancebar/releases/download/v#{version}/Balance-Bar-#{version}-mac.zip"
 
   appcast "https://github.com/scottrobertson/balancebar/releases.atom"
